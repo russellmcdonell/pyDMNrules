0278e2d20c76206fd1b473f255fc0eddb807368d
Russell McDonell <russell.mcdonell@c-cost.com>
2021-10-12 17:49:11 +1100

    Changing requirement.txt for readthedocs.io

--- pyDMNrules/setup.py
+++ pyDMNrules/setup.py
@@ -19,6 +19,6 @@ setuptools.setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
-    install_requires=['re', 'datetime', 'pySFeel','openpyxl'],
+    install_requires=['re', 'csv', 'datetime', 'pySFeel','openpyxl'],
 )
 
